ae00d901f942b9dadb3ed73f14eea5bc9523d780
francisco wallison <franciscowallison@gmail.com>
2025-06-06 15:20:06 -0300

    add full build

--- index.ts
+++
@@ -1,3 +0,0 @@
-export {GrfBrowser} from './src/grf-browser';
-export {GrfNode} from './src/grf-node';
-export type {TFileEntry} from './src/grf-base';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {GrfBrowser} from './grf-browser';
+export {GrfNode} from './grf-node';
+export type {TFileEntry} from './grf-base';
