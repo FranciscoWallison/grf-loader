e4ca24f7838efd1b38eea4cc70fc2b5a5c171006
francisco wallison <franciscowallison@gmail.com>
2025-06-11 11:14:39 -0300

    init configurações de build

--- tests/exports.test.js
+++ tests/exports.test.js
@@ -1,12 +1,12 @@
 describe('package entry points', () => {
   it('can be imported using ESM', async () => {
-    const mod = await import('../dist/esm/grf-loader.js');
+    const mod = await import('../dist/index.js');
     expect(mod.GrfBrowser).toBeDefined();
   });
 
   it('can be required using CJS', () => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const mod = require('../dist/cjs/grf-loader.cjs');
+    const mod = require('../dist/index.cjs');
     expect(mod.GrfBrowser).toBeDefined();
   });
 });
